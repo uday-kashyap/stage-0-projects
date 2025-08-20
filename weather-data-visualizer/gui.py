2893b7e8b046de7ff38558479ac03e85d0da319a
kashyapuday <kashyapuday063@gmail.com>
2025-08-20 17:17:48 +0530

    feat: design forecasting functionality to obtain temperature and humidity graphs using GUI

--- weather-data-visualizer/gui.py
+++ weather-data-visualizer/gui.py
@@ -5,6 +5,7 @@ from PIL import Image, ImageTk
 from weather_utils import Weather, Forecast
 from utils.config import get_api_key
 
+
 class WeatherApp(Tk):
 
     def __init__(self):
@@ -19,32 +20,48 @@ class WeatherApp(Tk):
         self.load_feature_icons()
         self.feature_ui()
 
-    def load_panels(self):
-        self.feature_panel = Frame(self, bg="lightgrey", borderwidth=2, relief=SOLID, width=50)
+    def load_panels(self) -> None:
+        self.feature_panel = Frame(
+            self, bg="lightgrey", borderwidth=2, relief=SOLID, width=50
+        )
         self.feature_panel.pack(side=LEFT, fill=Y)
         self.feature_panel.pack_propagate(False)
         self.info_panel = Frame(self, bg="white", borderwidth=2, relief=SOLID)
         self.info_panel.pack(fill=BOTH, expand=True)
         self.searchbox_panel = Frame(self.info_panel, bg="white")
         self.searchbox_panel.pack(side=TOP)
-        self.info_layout = Frame(self.info_panel, bg="SystemButtonFace", relief=SUNKEN, borderwidth=2)
-        self.info_layout.pack(fill=BOTH, expand=True, padx= 10, pady=10)
+        self.info_layout = Frame(
+            self.info_panel, bg="SystemButtonFace", relief=SUNKEN, borderwidth=2
+        )
+        self.info_layout.pack(fill=BOTH, expand=True, padx=10, pady=10)
 
-    def load_feature_icons(self):
-        weather_img = Image.open("weather_feature.png").resize((40,40), Image.LANCZOS)
+    def load_feature_icons(self) -> None:
+        weather_img = Image.open("weather_feature.png").resize((40, 40), Image.LANCZOS)
         self.weather_icon = ImageTk.PhotoImage(weather_img)
-        self.weather_button = Button(self.feature_panel, image=self.weather_icon, command=lambda: self.set_mode("Weather"))
+        self.weather_button = Button(
+            self.feature_panel,
+            image=self.weather_icon,
+            command=lambda: self.set_mode("Weather"),
+        )
         self.weather_button.pack(side=TOP, fill=Y, pady=5, padx=3)
-        plot_img = Image.open("plot_feature.png").resize((35,35), Image.LANCZOS)
+        plot_img = Image.open("plot_feature.png").resize((35, 35), Image.LANCZOS)
         self.plot_icon = ImageTk.PhotoImage(plot_img)
-        self.plot_button = Button(self.feature_panel, image=self.plot_icon, command=lambda: self.set_mode("Forecast"))
+        self.plot_button = Button(
+            self.feature_panel,
+            image=self.plot_icon,
+            command=lambda: self.set_mode("Forecast"),
+        )
         self.plot_button.pack(side=TOP, fill=Y, padx=3)
 
     def set_mode(self, mode: str):
         self.active_mode = mode
         self.search_button.config(text=f"Get {self.active_mode}")
         self.feature_ui()
-        
+
+    def clear_info_layout(self):
+        for widget in self.info_layout.winfo_children():
+            widget.destroy()
+
     def feature_ui(self):
         if self.active_mode == "Weather":
             self.weather_button.config(bg="white")
@@ -56,72 +73,154 @@ class WeatherApp(Tk):
     def weather_info_ui(self, status: str, error_message=None):
 
         def customize_temperature_font(temp_val, row_pos, col_pos=1):
-            if temp_val>=30:
-                Label(self.info_layout, text=f"{temp_val}°C", font="lucida 10 bold", fg="red").grid(row=row_pos, column=col_pos)
-            elif temp_val>25:
-                Label(self.info_layout, text=f"{temp_val}°C", font="lucida 10 bold", fg="orange").grid(row=row_pos, column=col_pos)
-            elif temp_val<=7:
-                Label(self.info_layout, text=f"{temp_val}°C", font="lucida 10 bold", fg="blue").grid(row=row_pos, column=col_pos)
+            if temp_val >= 30:
+                Label(
+                    self.info_layout,
+                    text=f"{temp_val}°C",
+                    font="lucida 10 bold",
+                    fg="red",
+                ).grid(row=row_pos, column=col_pos)
+            elif temp_val > 25:
+                Label(
+                    self.info_layout,
+                    text=f"{temp_val}°C",
+                    font="lucida 10 bold",
+                    fg="orange",
+                ).grid(row=row_pos, column=col_pos)
+            elif temp_val <= 7:
+                Label(
+                    self.info_layout,
+                    text=f"{temp_val}°C",
+                    font="lucida 10 bold",
+                    fg="blue",
+                ).grid(row=row_pos, column=col_pos)
             else:
-                Label(self.info_layout, text=f"{temp_val}°C", font="lucida 10 bold", fg="#524309").grid(row=row_pos, column=col_pos)
+                Label(
+                    self.info_layout,
+                    text=f"{temp_val}°C",
+                    font="lucida 10 bold",
+                    fg="#524309",
+                ).grid(row=row_pos, column=col_pos)
 
-        if status == "Success":
+        try:
+            if status == "Success":
 
-            # Set and display the information in UI
-            Label(self.info_layout, text=f"Current Weather:", font="lucida 10 bold").grid(row=0, column=0, sticky="w")
-            customize_temperature_font(self.temperature, row_pos=0)
-            Label(self.info_layout, text=f"Condition:", font="lucida 10 bold").grid(row=1, column=0, sticky="w")
-            Label(self.info_layout, text=f"{self.condition}", font="agencyfb 10 bold").grid(row=1,column=1)
-            Label(self.info_layout, text=f"Feels Like:", font="lucida 10 bold").grid(row=2, column=0, sticky="w")
-            customize_temperature_font(self.feels_like, row_pos=2)
-            Label(self.info_layout, text=f"Humidity:", font="lucida 10 bold").grid(row=3, column=0, sticky="w")
-            Label(self.info_layout, text=f"{self.humidity}%", font="agencyfb 10 bold").grid(row=3, column=1)
+                # Set and display the information on UI
+                Label(
+                    self.info_layout, text="Current Weather:", font="lucida 10 bold"
+                ).grid(row=0, column=0, sticky="w")
+                customize_temperature_font(self.temperature, row_pos=0)
+                Label(self.info_layout, text="Condition:", font="lucida 10 bold").grid(
+                    row=1, column=0, sticky="w"
+                )
+                Label(
+                    self.info_layout, text=f"{self.condition}", font="agencyfb 10 bold"
+                ).grid(row=1, column=1)
+                Label(
+                    self.info_layout, text="Feels Like:", font="lucida 10 bold"
+                ).grid(row=2, column=0, sticky="w")
+                customize_temperature_font(self.feels_like, row_pos=2)
+                Label(self.info_layout, text="Humidity:", font="lucida 10 bold").grid(
+                    row=3, column=0, sticky="w"
+                )
+                Label(
+                    self.info_layout, text=f"{self.humidity}%", font="agencyfb 10 bold"
+                ).grid(row=3, column=1)
 
-        else:
-            error_message = Label(self.info_layout, text=error_message)
-            error_message.pack()
+            else:
+                error_message = Label(self.info_layout, text=error_message)
+                error_message.pack()
+        finally:
 
+            # Restore original stdout
+            sys.stdout = sys_stdout
 
-    def forecast_info_ui(self):
-        pass
+    def forecast_choices_ui(self):
+        def fetch_temperature_plot():
+
+            # Clear previous content
+            self.clear_info_layout()
+
+            try:
+                self.forecast.plot_forecast(
+                    city=self.city_name.get(), type="temperature"
+                )
+            except Exception as e:
+
+                # Handle errors
+                error_message = buffer.getvalue().strip()
+                Label(self.info_layout, text=error_message).pack()
+
+            finally:
+
+                # Restore original stdout
+                sys.stdout = sys_stdout
+
+        def fetch_humidity_plot():
+
+            # Clear previous content
+            self.clear_info_layout()
+
+            try:
+                self.forecast.plot_forecast(city=self.city_name.get(), type="humidity")
+            except:
+
+                # Handle errors
+                error_message = buffer.getvalue().strip()
+                Label(self.info_layout, text=error_message).pack()
+
+            finally:
+
+                # Restore original stdout
+                sys.stdout = sys_stdout
+
+        plot_temp_button = Button(
+            self.info_layout, text="Plot Temperature", command=fetch_temperature_plot
+        )
+        plot_temp_button.pack(pady=10)
+        plot_humidity_button = Button(
+            self.info_layout,
+            text="Plot Humidity",
+            width=13,
+            command=fetch_humidity_plot,
+        )
+        plot_humidity_button.pack()
 
     def get_info(self):
 
         # Clear previous content
-        for widget in self.info_layout.winfo_children():
-            widget.destroy()
+        self.clear_info_layout()
 
         API_KEY = get_api_key()
 
         # Error handling on GUI logic
+        global buffer
         buffer = io.StringIO()
+        global sys_stdout
         sys_stdout = sys.stdout
         sys.stdout = buffer
 
-        try:
-            if self.active_mode == "Weather":
+        if self.active_mode == "Weather":
 
-                # Fetch the weather data
-                weather = Weather(API_KEY)
-                weather_data = weather.get_weather(self.city_name.get())
+            # Fetch the weather data
+            weather = Weather(API_KEY)
+            weather_data = weather.get_weather(self.city_name.get())
 
-                if weather_data:
-                    self.city = weather_data[0]
-                    self.country_code = weather_data[1]
-                    self.temperature = weather_data[2]
-                    self.condition = weather_data[3]
-                    self.feels_like = weather_data[4]
-                    self.humidity = weather_data[5]
-                    self.weather_info_ui(status="Success")
-                else:
-                    self.weather_info_ui(status="Error", error_message=buffer.getvalue().strip())
+            if weather_data:
+                self.city = weather_data[0]
+                self.country_code = weather_data[1]
+                self.temperature = weather_data[2]
+                self.condition = weather_data[3]
+                self.feels_like = weather_data[4]
+                self.humidity = weather_data[5]
+                self.weather_info_ui(status="Success")
             else:
-                #TODO: forecast plotting logic
-                pass
-
-        # Restore real stdout
-        finally:
-            sys.stdout = sys_stdout
+                self.weather_info_ui(
+                    status="Error", error_message=buffer.getvalue().strip()
+                )
+        else:
+            self.forecast = Forecast(API_KEY)
+            self.forecast_choices_ui()
 
     def display_searchbox(self):
 
@@ -131,13 +230,19 @@ class WeatherApp(Tk):
 
         self.city_name = StringVar()
         self.city_name.set("Search for your city")
-        self.city_searchbox = Entry(self.searchbox_panel, textvariable=self.city_name, borderwidth=2, relief=SOLID)
+        self.city_searchbox = Entry(
+            self.searchbox_panel,
+            textvariable=self.city_name,
+            borderwidth=2,
+            relief=SOLID,
+        )
         self.city_searchbox.pack(side=LEFT, pady=5, ipady=2)
         self.city_searchbox.bind("<Button-1>", clear_placeholder)
-        self.search_button = Button(self.searchbox_panel, text= f"Get {self.active_mode}", command=self.get_info)
+        self.search_button = Button(
+            self.searchbox_panel, text=f"Get {self.active_mode}", command=self.get_info
+        )
         self.search_button.pack(side=LEFT, padx=3)
 
 
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     WeatherApp().mainloop()
