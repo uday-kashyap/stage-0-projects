75738a5b645a755748a0e88884cb4372fa747593
kashyapuday <kashyapuday063@gmail.com>
2026-06-03 11:58:54 +0530

    feat: modularize code structure and implement multiple accounts functionality

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,29 +1,75 @@
 import json
 
-# Input Credentials
-website = input("Enter your website (e.g., Github, Google, Apple etc): ")
-username = input("Enter your username: ")
-password = input("Enter your password: ")
 
-# Load data
-with open("passwords.json", "r") as file:
-    data = json.load(file)
+def get_credentials():
+    '''
+    Input user credentials.
+    '''
 
-    # Handle existing data
-    if (website in data) and (data[website]["username"] == username):
-        print("Account already exists with the same username!")
-        exit()
+    website = input("Enter your website (e.g., Github, Google, Apple etc): ")
+    username = input("Enter your username: ")
+    password = input("Enter your password: ")
 
-# Store data
-data = {
-    website: {
+    return website, username, password
+
+
+def load_data():
+    '''
+    Load existing data.
+    '''
+    
+    try:
+        with open("passwords.json", "r") as file:
+            return json.load(file)
+        
+    except (FileNotFoundError, json.JSONDecodeError):
+        return {}
+    
+
+def is_account_duplicate(website, username, data):
+    '''
+    Check for the pre-existing account with the same username and website name.
+    '''
+
+    if website in data:
+        
+        for credentials in data[website]:
+
+            if credentials["username"] == username:
+                return True
+    
+    return False
+
+
+def save_data(website, username, password, data):
+    '''
+    Store and save the user credentials.
+    '''
+
+    if website not in data:
+        data[website] = []
+
+    data[website].append({
         "username": username,
         "password": password
-    }
-}
+    })
 
-# Save data
-with open("passwords.json", "w") as file:
-    json.dump(data, file, indent=4)
+    with open("passwords.json", "w") as file:
+        json.dump(data, file, indent=4)
     
-print("Your credentials have been saved successfully.")
+    print("Your credentials have been saved successfully.")
+
+
+def main():
+    website, username, password = get_credentials()
+    data = load_data()
+    account_exists = is_account_duplicate(website, username, data)
+
+    if account_exists:
+        print("Account already exists with the same username!")
+        return
+    
+    save_data(website, username, password, data)
+
+if __name__ == '__main__':
+    main()
