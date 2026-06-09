11861190ad0ea76d0a66ddb9df11138cd4ea402b
kashyapuday <kashyapuday063@gmail.com>
2026-06-09 17:40:13 +0530

    style: format code with black

--- password-manager/main.py
+++ password-manager/main.py
@@ -1,19 +1,18 @@
 import json
 
-
 FILE_NAME = "passwords.json"
 FEATURES = {
-        1: "Add account",
-        2: "Search account",
-        3: "Delete account",
-        4: "Update account"
-    }
+    1: "Add account",
+    2: "Search account",
+    3: "Delete account",
+    4: "Update account",
+}
 
 
 def add_account(data: dict) -> None:
-    '''
+    """
     Add a new user account.
-    '''
+    """
 
     website, username, password = get_credentials()
     print()
@@ -23,22 +22,22 @@ def add_account(data: dict) -> None:
     if account_idx is not None:
         print("Account already exists with the same username!")
         return
-        
+
     create_account(website, username, password, data)
     save_to_JSON_file(FILE_NAME, data)
     print("Your account has been created successfully.")
 
 
 def search_account(data: dict) -> None:
-    '''
+    """
     Search for existing user account.
-    '''
+    """
 
     website, username = get_website_and_username()
     print()
-    
+
     account_idx = fetch_account_index(website, username, data)
-        
+
     if account_idx is not None:
 
         for attribute, attribute_val in data[website][account_idx].items():
@@ -49,13 +48,13 @@ def search_account(data: dict) -> None:
 
 
 def delete_account(data: dict) -> None:
-    '''
+    """
     Delete existing user account.
-    '''
+    """
 
     website, username, password = get_credentials()
     print()
-    
+
     account_idx = fetch_account_index(website, username, data)
 
     if account_idx is not None:
@@ -80,9 +79,9 @@ def delete_account(data: dict) -> None:
 
 
 def update_account(data: dict) -> None:
-    '''
+    """
     Update existing user account.
-    '''
+    """
 
     website, username, password = get_credentials()
     print()
@@ -94,45 +93,45 @@ def update_account(data: dict) -> None:
         valid_password = validate_password(website, password, data, account_idx)
 
         if valid_password:
-            
+
             new_username = input("Enter a new username: ")
             new_password = input("Enter a new password: ")
             print()
 
             target_account = data[website][account_idx]
-                
+
             # Update attributes
             target_account["username"] = new_username
             target_account["password"] = new_password
 
             save_to_JSON_file(FILE_NAME, data)
             print("Your account has been updated successfully.")
-                
+
         else:
             print("Invalid password!")
-                    
+
     else:
         print("Account does not exist!")
 
 
 def validate_password(website: str, password: str, data: dict, account_idx: int) -> bool:
-    '''
+    """
     Validate provided password to the actual password of the account.
     Return True if matches, else False.
-    '''
+    """
 
     target_account = data[website][account_idx]
 
     if target_account["password"] == password:
         return True
-    
+
     return False
 
 
 def save_to_JSON_file(file_name: str, data: dict) -> None:
-    '''
+    """
     Save all the modified, added or deleted changes to the data containing file.
-    '''
+    """
 
     try:
         with open(file_name, "w") as file:
@@ -143,18 +142,18 @@ def save_to_JSON_file(file_name: str, data: dict) -> None:
 
 
 def display_menu() -> None:
-    '''
+    """
     Show system features menu to the user.
-    '''
+    """
 
     for option_no, feature in FEATURES.items():
         print(f"{option_no}. {feature}")
 
 
 def get_user_choice() -> int:
-    '''
+    """
     Take and validate user choice. Return it after validation.
-    '''
+    """
 
     while True:
         try:
@@ -164,16 +163,16 @@ def get_user_choice() -> int:
                 print("Please enter a valid option!")
                 continue
 
-            return choice # Return a valid choice
-        
+            return choice  # Return a valid choice
+
         except ValueError:
             print("Options must be integers!")
 
 
 def get_credentials() -> tuple[str, str, str]:
-    '''
+    """
     Input and return user credentials.
-    '''
+    """
 
     website = input("Enter your website (e.g., Github, Google, Apple etc): ")
     username = input("Enter your username: ")
@@ -183,9 +182,9 @@ def get_credentials() -> tuple[str, str, str]:
 
 
 def get_website_and_username() -> tuple[str, str]:
-    '''
+    """
     Input and return 'website' and 'username'.
-    '''
+    """
 
     website = input("Enter your website (e.g., Github, Google, Apple etc): ")
     username = input("Enter your username: ")
@@ -194,27 +193,24 @@ def get_website_and_username() -> tuple[str, str]:
 
 
 def load_data_from_JSON_file(file_name: str) -> dict:
-    '''
+    """
     Load and return existing data.
-    '''
-    
+    """
+
     try:
         with open(file_name, "r") as file:
             return json.load(file)
-        
+
     except (FileNotFoundError, json.JSONDecodeError):
         return {}
 
 
 def create_account(website: str, username: str, password: str, data: dict) -> None:
-    '''
+    """
     Create a new user account.
-    '''
+    """
 
-    new_account = {
-        "username": username,
-        "password": password
-    }
+    new_account = {"username": username, "password": password}
 
     # Root dictionary modifications
     if website not in data:
@@ -224,17 +220,17 @@ def create_account(website: str, username: str, password: str, data: dict) -> No
 
 
 def fetch_account_index(website: str, username: str, data: dict) -> int | None:
-    '''
+    """
     Fetch user account's index and return it.
-    '''
+    """
 
     if website in data:
-    
+
         for idx, account in enumerate(data[website]):
 
             if account["username"] == username:
                 return idx
-    
+
     return None
 
 
@@ -258,5 +254,5 @@ def main() -> None:
         update_account(data)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
